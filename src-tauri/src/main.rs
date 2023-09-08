26ab13090cd20d9d5008af8e43a36e0022e31531
Revolution <tavinho.daniel@gmail.com>
2023-09-08 18:19:09 -0300

    refactor: remove unused Greet component and test function

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[tauri::command]
-fn test(name: &str) -> String {
-	format!("Hello {}", name)
-}
-
 fn main() {
 	tauri::Builder::default()
-		.invoke_handler(tauri::generate_handler![test])
 		.run(tauri::generate_context!())
 		.expect("error while running tauri application");
 }
