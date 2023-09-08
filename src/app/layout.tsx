3ecbf79e4940c8fb4f7100fc5479bbeb77c023b3
Revolution <tavinho.daniel@gmail.com>
2023-09-08 18:28:00 -0300

    refactor: theme and emotion-cache paths for better organization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 import "./globals.css";
-import ThemeRegister from "@/components/ThemeRegister";
 import React from "react";
+import ThemeRegistry from "@/theme/theme-registry";
 
 export default function RootLayout({
 	children,
@@ -11,7 +11,9 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body>
-				<ThemeRegister>{children}</ThemeRegister>
+				<ThemeRegistry>
+					{children}
+				</ThemeRegistry>
 			</body>
 		</html>
 	);
