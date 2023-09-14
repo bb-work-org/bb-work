610be685e350372a149c8fc58b3a6e60cdbacbee
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-09-14 19:49:21 -0300

    feat: show expire date

--- src/components/activity-card.tsx
+++ src/components/activity-card.tsx
@@ -1,5 +1,5 @@
-import { IconButton, ListItem, ListItemText, Tooltip } from "@mui/material";
-import { Done, Visibility } from "@mui/icons-material";
+import { ListItem, ListItemText, Tooltip } from "@mui/material";
+import { Done, QueryBuilder } from "@mui/icons-material";
 import { Activity } from "@/@types/activities";
 import { useGetAttemptsQuery } from "@/redux/services/activity-api";
 
@@ -12,25 +12,29 @@ export default function ActivityCard({ activity }: { activity: Activity }) {
 	return (
 		<ListItem key={activity.id} divider>
 			<ListItemText
-				primary={`${activity.title}`}
-				secondary={`${activity.id}`}
+				primary={activity.title}
+				secondary={activity.id}
 			/>
 
 			{
 				data && Object.keys(data.lookup).length !== 0
-					? (
+					  ? (
 						<Tooltip title={"Completed"}>
 							<Done/>
 						</Tooltip>
-					)
-					: undefined
+					  )
+					  : undefined
 			}
 
-			<Tooltip title={"Seen"}>
-				<IconButton>
-					<Visibility/>
-				</IconButton>
-			</Tooltip>
+			{
+				activity.adaptiveReleaseRules
+					? (
+						<Tooltip title={new Date(activity.adaptiveReleaseRules.endDate).toLocaleDateString()}>
+							<QueryBuilder />
+						</Tooltip>
+					  )
+					: undefined
+			}
 		</ListItem>
 	)
 }
