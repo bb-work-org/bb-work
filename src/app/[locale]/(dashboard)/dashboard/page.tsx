66530eb1bce5aa2d18536d6348946ff58e4d65d3
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-09-17 20:23:23 -0300

    feat: apply skeletons on loading

--- src/app/[locale]/(dashboard)/dashboard/page.tsx
+++ src/app/[locale]/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,8 @@
 "use client";
-import {CircularProgress, Grid, Stack, Typography} from "@mui/material";
+import {Grid, Stack} from "@mui/material";
 import {useGetMeQuery} from "@/redux/services/user-api";
 import {useGetCoursesQuery} from "@/redux/services/course-api";
-import CourseCard from "@/components/course-card";
+import {CourseCard} from "@/components/course-card";
 
 export default function Activities() {
 	const { isLoading, data } = useGetMeQuery();
@@ -17,17 +17,20 @@ export default function Activities() {
 			alignContent={"center"}
 			direction={"column"}
 			p={3}
-			my={2}
 		>
-			<Typography display={"flex"} flexDirection={"column"} alignItems={"center"} gap={2}>
-				Activities - { courses?.paging?.count ?? 0 }
-				{isLoading ? <CircularProgress/> : undefined}
-			</Typography>
-
 			<Grid spacing={2} my={1} container>
-				{courses?.results?.map((courseRoot) => (
-					<CourseCard key={courseRoot.id} courseRoot={courseRoot} />
-				))}
+				{isLoading
+					? (
+						new Array(8).fill(null).map((index) => (
+							<CourseCard.Skeleton key={index} />
+						))
+					)
+					: (
+						courses?.results?.map((courseRoot) => (
+							<CourseCard.Root key={courseRoot.id} courseRoot={courseRoot} />
+						))
+					)
+				}
 			</Grid>
 		</Stack>
 	);
--- src/components/course-card.tsx
+++ src/components/course-card.tsx
@@ -1,12 +1,26 @@
-import {ButtonBase, Card, CardContent, CardMedia, Grid, Typography} from "@mui/material";
+import {ButtonBase, Card, CardContent, CardMedia, Grid, Skeleton as MuiSkeleton, Typography} from "@mui/material";
 import {CourseResult} from "@/@types/courses";
 import Image from "next/image";
 import {useRouter} from "next/navigation";
-import {useEffect, useState} from "react";
+import React, {PropsWithChildren, ReactNode, useEffect, useState} from "react";
 import {useGetCourseBannerQuery, useGetCourseStaticBannerQuery} from "@/redux/services/banner-api";
 import {useWithLocale} from "@/hooks/useWithLocale";
 
-export default function CourseCard({ courseRoot }: { courseRoot: CourseResult }) {
+function Skeleton() {
+	const imageSkeleton = (
+		<MuiSkeleton variant={"rectangular"} height={"100%"}/>
+	);
+
+	return (
+		<Body expired={false} handleClick={() => {}} image={imageSkeleton}>
+			<MuiSkeleton variant={"text"} width={"25%"}/>
+			<MuiSkeleton variant={"text"} width={"45%"}/>
+			<MuiSkeleton variant={"text"} width={"30%"}/>
+		</Body>
+	)
+}
+
+function Root({ courseRoot }: { courseRoot: CourseResult }) {
 	const router = useRouter();
 	const withLocale = useWithLocale();
 	const [expired, setExpired] = useState(false);
@@ -32,18 +46,42 @@ export default function CourseCard({ courseRoot }: { courseRoot: CourseResult })
 	}, [courseRoot.course.term?.endDate]);
 
 	return (
-		<Grid sx={{ opacity: expired ? 0.5 : 1 }} xs={12} sm={12} md={6} lg={3} item>
-			<ButtonBase disabled={expired} onClick={() => handleClick()} sx={{ width: "100%", height: "100%" }}>
+		<Body expired={expired} handleClick={handleClick} image={bannerUrl ? (
+			<Image
+				src={bannerUrl}
+				alt={courseRoot.course.name}
+				layout={"fill"}
+				objectFit={"cover"}
+			/>
+		) : undefined}>
+			<Typography textAlign={"left"} sx={{ fontSize: 11 }} color={"text.secondary"} gutterBottom>
+				{courseRoot.courseId}
+			</Typography>
+
+			<Typography textAlign={"left"} sx={{ fontSize: 15 }}>
+				{courseRoot.course.name}
+			</Typography>
+
+			<Typography textAlign={"left"} sx={{ fontSize: 12 }} color={"text.secondary"}>
+				{courseRoot.course.description}
+			</Typography>
+		</Body>
+	)
+}
+
+function Body({ expired, handleClick, image, clickable, children }: PropsWithChildren<{
+	expired: boolean;
+	handleClick: () => void;
+	image: ReactNode;
+	clickable?: boolean;
+}>) {
+	return (
+		<Grid xs={12} sm={12} md={6} lg={3} item>
+			<ButtonBase disabled={expired && clickable} onClick={() => handleClick()} sx={{ width: "100%", height: "100%" }}>
 				<Card sx={{ width: "100%", height: "100%" }}>
 					<CardMedia sx={{ height: "7rem" }}>
 						<div style={{ position: 'relative', width: '100%', height: '100%' }}>
-							<Image
-								src={ bannerUrl ?? "" }
-								style={{ width: "100%", height: "100%", objectFit: "cover" }}
-								alt={courseRoot.course.bannerAltText ?? "Banner"}
-								width={0}
-								height={0}
-							/>
+							{image}
 						</div>
 					</CardMedia>
 
@@ -52,20 +90,15 @@ export default function CourseCard({ courseRoot }: { courseRoot: CourseResult })
 						flexDirection: "column",
 						alignItems: "flex-start"
 					}}>
-						<Typography textAlign={"left"} sx={{ fontSize: 11 }} color={"text.secondary"} gutterBottom>
-							{courseRoot.courseId}
-						</Typography>
-
-						<Typography textAlign={"left"} sx={{ fontSize: 15 }}>
-							{courseRoot.course.name}
-						</Typography>
-
-						<Typography textAlign={"left"} sx={{ fontSize: 12 }} color={"text.secondary"}>
-							{courseRoot.course.description}
-						</Typography>
+						{children}
 					</CardContent>
 				</Card>
 			</ButtonBase>
 		</Grid>
 	)
+}
+
+export const CourseCard = {
+	Root,
+	Skeleton
 }
