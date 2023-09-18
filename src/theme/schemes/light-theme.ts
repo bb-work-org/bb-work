05e69caf21d85f0391a59622d711745c913bf5b1
Revolution <tavinho.daniel@gmail.com>
2023-09-17 23:27:05 -0300

    feat: add scrollbar custom styles to light-theme and dark-theme

--- src/theme/schemes/light-theme.ts
+++ src/theme/schemes/light-theme.ts
@@ -1,7 +1,42 @@
-import {createTheme} from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles";
 
 export const lightTheme = createTheme({
-    palette: {
-        mode: 'light',
-    }
+	palette: {
+		mode: "light",
+	},
+	components: {
+		MuiCssBaseline: {
+			styleOverrides: {
+				body: {
+					scrollbarColor: "#272727 #121212",
+					"&::-webkit-scrollbar, & *::-webkit-scrollbar": {
+						width: 12,
+					},
+					"&::-webkit-scrollbar-thumb, & *::-webkit-scrollbar-thumb":
+						{
+							borderRadius: 8,
+							backgroundColor: "#6b6b6b",
+							minHeight: 24,
+							border: "3px solid #121212",
+						},
+					"&::-webkit-scrollbar-thumb:focus, & *::-webkit-scrollbar-thumb:focus":
+						{
+							backgroundColor: "#959595",
+						},
+					"&::-webkit-scrollbar-thumb:active, & *::-webkit-scrollbar-thumb:active":
+						{
+							backgroundColor: "#959595",
+						},
+					"&::-webkit-scrollbar-thumb:hover, & *::-webkit-scrollbar-thumb:hover":
+						{
+							backgroundColor: "#959595",
+						},
+					"&::-webkit-scrollbar-corner, & *::-webkit-scrollbar-corner":
+						{
+							backgroundColor: "#2b2b2b",
+						},
+				},
+			},
+		},
+	},
 });
