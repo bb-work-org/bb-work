b01dd93988abd4072a47faf3787a5a4a8307cd81
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-09-20 08:52:10 -0300

    chore: remove unused activity api query

--- src/redux/services/activity-api.ts
+++ src/redux/services/activity-api.ts
@@ -1,21 +1,6 @@
 import {createApi} from "@reduxjs/toolkit/query/react";
 import {authFetchBaseQuery} from "@/utils/auth-fetch-base-query";
 import {getApi} from "@/utils/get-api";
-import {Body, HttpVerb} from "@tauri-apps/api/http";
-import {ActivityResult} from "@/@types/activities";
-
-interface ActivityRequest {
-	method: HttpVerb;
-	relativeUrl: string;
-}
-
-interface ActivityResponse extends ActivityRequest {
-	body: ActivityResult;
-	statusCode: number;
-	headers: {
-		[key: string]: string[];
-	}
-}
 
 export const activityApi = createApi({
 	reducerPath: "activityApi",
@@ -25,17 +10,8 @@ export const activityApi = createApi({
 			query: (data) => ({
 				url: `/learn/api/v1/courses/${data.courseId}/gradebook/columns/${data.activityId}/attempts`
 			})
-		}),
-		getActivities: builder.query<ActivityResponse[], ActivityRequest[]>({
-			query: (activitiesRequests) => ({
-				url: "/learn/api/v1/utilities/batch",
-				options: {
-					body: Body.json(activitiesRequests),
-					method: "PUT"
-				}
-			})
 		})
 	})
 });
 
-export const { useGetAttemptsQuery, useGetActivitiesQuery } = activityApi;
+export const { useGetAttemptsQuery } = activityApi;
