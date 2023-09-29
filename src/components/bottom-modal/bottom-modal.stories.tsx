1bf9150c04d7ed55a8c76e5cc777831eb86c3d79
Revolution <tavinho.daniel@gmail.com>
2023-09-28 22:41:23 -0300

    refactor: improved content in BottomModal storybook

--- src/components/bottom-modal/bottom-modal.stories.tsx
+++ src/components/bottom-modal/bottom-modal.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta } from "@storybook/react";
 import { BottomModal } from "@/components/bottom-modal/bottom-modal";
+import { List, ListItem, ListItemText } from "@mui/material";
 
 const meta: Meta<typeof BottomModal> = {
 	title: "Components/BottomModal",
@@ -8,7 +9,17 @@ const meta: Meta<typeof BottomModal> = {
 
 export const Default = () => (
 	<BottomModal open={true}>
-		<div>Content</div>
+		<List>
+			<ListItem>
+				<ListItemText>Item 1</ListItemText>
+			</ListItem>
+			<ListItem>
+				<ListItemText>Item 2</ListItemText>
+			</ListItem>
+			<ListItem>
+				<ListItemText>Item 3</ListItemText>
+			</ListItem>
+		</List>
 	</BottomModal>
 );
 
